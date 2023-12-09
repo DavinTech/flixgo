ea172323c3593980a40438d292d0e8a44b4515aa
DavinTech <148042975+DavinTech@users.noreply.github.com>
2023-12-09 19:55:58 +0900

    Add top page #2

--- src/main/java/com/example/flixgo/FlixgoApplication.java
+++ src/main/java/com/example/flixgo/FlixgoApplication.java
@@ -2,12 +2,18 @@ package com.example.flixgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlixgoApplication {
+public class FlixgoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlixgoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FlixgoApplication.class);
+	}
 }
