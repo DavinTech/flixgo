f02b503d47c6f1aac0bfa5e9ea49503c970fa057
DavinTech <148042975+DavinTech@users.noreply.github.com>
2023-12-10 21:04:13 +0900

    Add signup and login page #3

---
+++ src/main/java/com/example/flixgo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.flixgo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.flixgo.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByUsername(String username);
+}
